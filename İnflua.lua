bf3616ef598faadc5858ea964e378f9714c6a135
xenesdev <142043896+xenesdev@users.noreply.github.com>
2023-08-14 19:25:55 +0300

    Update 429 İnflua.lua
    ım so tireds

--- İnflua.lua
+++ İnflua.lua
@@ -235,3 +235,195 @@ do
 		Title.Text = ("%s %s %s"):format(emoji, Title.Text, emoji)
 	end
 end
+
+Title.TextColor3 = Color3.new(1, 1, 1)
+Title.ZIndex = 10
+table.insert(shade1,Title)
+table.insert(text1,Title)
+
+Dark.Name = "Dark"
+Dark.Parent = Holder
+Dark.Active = true
+Dark.BackgroundColor3 = Color3.fromRGB(36, 36, 37)
+Dark.BorderSizePixel = 0
+Dark.Position = UDim2.new(0, 0, 0, 45)
+Dark.Size = UDim2.new(0, 250, 0, 175)
+Dark.ZIndex = 10
+table.insert(shade1,Dark)
+
+Cmdbar.Name = "Cmdbar"
+Cmdbar.Parent = Holder
+Cmdbar.BackgroundTransparency = 1
+Cmdbar.BorderSizePixel = 0
+Cmdbar.Position = UDim2.new(0, 5, 0, 20)
+Cmdbar.Size = UDim2.new(0, 240, 0, 25)
+Cmdbar.Font = Enum.Font.SourceSans
+Cmdbar.TextSize = 18
+Cmdbar.TextXAlignment = Enum.TextXAlignment.Left
+Cmdbar.TextColor3 = Color3.new(1, 1, 1)
+Cmdbar.Text = ""
+Cmdbar.ZIndex = 10
+Cmdbar.PlaceholderText = "Command Bar"
+
+CMDsF.Name = "CMDs"
+CMDsF.Parent = Holder
+CMDsF.BackgroundTransparency = 1
+CMDsF.BorderSizePixel = 0
+CMDsF.Position = UDim2.new(0, 5, 0, 45)
+CMDsF.Size = UDim2.new(0, 245, 0, 175)
+CMDsF.ScrollBarImageColor3 = Color3.fromRGB(78,78,79)
+CMDsF.BottomImage = "rbxasset://textures/ui/Scroll/scroll-middle.png"
+CMDsF.CanvasSize = UDim2.new(0, 0, 0, 0)
+CMDsF.MidImage = "rbxasset://textures/ui/Scroll/scroll-middle.png"
+CMDsF.ScrollBarThickness = 8
+CMDsF.TopImage = "rbxasset://textures/ui/Scroll/scroll-middle.png"
+CMDsF.VerticalScrollBarInset = 'Always'
+CMDsF.ZIndex = 10
+table.insert(scroll,CMDsF)
+
+cmdListLayout.Parent = CMDsF
+
+SettingsButton.Name = "SettingsButton"
+SettingsButton.Parent = Holder
+SettingsButton.BackgroundTransparency = 1
+SettingsButton.Position = UDim2.new(0, 230, 0, 0)
+SettingsButton.Size = UDim2.new(0, 20, 0, 20)
+SettingsButton.Image = "rbxassetid://1204397029"
+SettingsButton.ZIndex = 10
+
+ReferenceButton = Instance.new("ImageButton")
+ReferenceButton.Name = "ReferenceButton"
+ReferenceButton.Parent = Holder
+ReferenceButton.BackgroundTransparency = 1
+ReferenceButton.Position = UDim2.new(0, 212, 0, 2)
+ReferenceButton.Size = UDim2.new(0, 16, 0, 16)
+ReferenceButton.Image = "rbxassetid://3523243755"
+ReferenceButton.ZIndex = 10
+
+Settings.Name = "Settings"
+Settings.Parent = Holder
+Settings.Active = true
+Settings.BackgroundColor3 = Color3.fromRGB(36, 36, 37)
+Settings.BorderSizePixel = 0
+Settings.Position = UDim2.new(0, 0, 0, 220)
+Settings.Size = UDim2.new(0, 250, 0, 175)
+Settings.ZIndex = 10
+table.insert(shade1,Settings)
+
+SettingsHolder = Instance.new("ScrollingFrame")
+SettingsHolder.Name = "Holder"
+SettingsHolder.Parent = Settings
+SettingsHolder.BackgroundTransparency = 1
+SettingsHolder.BorderSizePixel = 0
+SettingsHolder.Size = UDim2.new(1,0,1,0)
+SettingsHolder.ScrollBarImageColor3 = Color3.fromRGB(78,78,79)
+SettingsHolder.BottomImage = "rbxasset://textures/ui/Scroll/scroll-middle.png"
+SettingsHolder.CanvasSize = UDim2.new(0, 0, 0, 235)
+SettingsHolder.MidImage = "rbxasset://textures/ui/Scroll/scroll-middle.png"
+SettingsHolder.ScrollBarThickness = 8
+SettingsHolder.TopImage = "rbxasset://textures/ui/Scroll/scroll-middle.png"
+SettingsHolder.VerticalScrollBarInset = 'Always'
+SettingsHolder.ZIndex = 10
+table.insert(scroll,SettingsHolder)
+
+Prefix.Name = "Prefix"
+Prefix.Parent = SettingsHolder
+Prefix.BackgroundColor3 = Color3.fromRGB(46, 46, 47)
+Prefix.BorderSizePixel = 0
+Prefix.BackgroundTransparency = 1
+Prefix.Position = UDim2.new(0, 5, 0, 5)
+Prefix.Size = UDim2.new(1, -10, 0, 20)
+Prefix.Font = Enum.Font.SourceSans
+Prefix.TextSize = 14
+Prefix.Text = "Prefix"
+Prefix.TextColor3 = Color3.new(1, 1, 1)
+Prefix.TextXAlignment = Enum.TextXAlignment.Left
+Prefix.ZIndex = 10
+table.insert(shade2,Prefix)
+table.insert(text1,Prefix)
+
+PrefixBox.Name = "PrefixBox"
+PrefixBox.Parent = Prefix
+PrefixBox.BackgroundColor3 = Color3.fromRGB(78, 78, 79)
+PrefixBox.BorderSizePixel = 0
+PrefixBox.Position = UDim2.new(1, -20, 0, 0)
+PrefixBox.Size = UDim2.new(0, 20, 0, 20)
+PrefixBox.Font = Enum.Font.SourceSansBold
+PrefixBox.TextSize = 14
+PrefixBox.Text = ''
+PrefixBox.TextColor3 = Color3.new(0, 0, 0)
+PrefixBox.ZIndex = 10
+table.insert(shade3,PrefixBox)
+table.insert(text2,PrefixBox)
+
+function makeSettingsButton(name,iconID,off)
+	local button = Instance.new("TextButton")
+	button.BackgroundColor3 = Color3.fromRGB(46, 46, 47)
+	button.BorderSizePixel = 0
+	button.Position = UDim2.new(0,0,0,0)
+	button.Size = UDim2.new(1,0,0,25)
+	button.Text = ""
+	button.ZIndex = 10
+	local icon = Instance.new("ImageLabel")
+	icon.Name = "Icon"
+	icon.Parent = button
+	icon.Position = UDim2.new(0,5,0,5)
+	icon.Size = UDim2.new(0,16,0,16)
+	icon.BackgroundTransparency = 1
+	icon.Image = iconID
+	icon.ZIndex = 10
+	if off then
+		icon.ScaleType = Enum.ScaleType.Crop
+		icon.ImageRectSize = Vector2.new(16,16)
+		icon.ImageRectOffset = Vector2.new(off,0)
+	end
+	local label = Instance.new("TextLabel")
+	label.Name = "ButtonLabel"
+	label.Parent = button
+	label.BackgroundTransparency = 1
+	label.Text = name
+	label.Position = UDim2.new(0,28,0,0)
+	label.Size = UDim2.new(1,-28,1,0)
+	label.Font = Enum.Font.SourceSans
+	label.TextColor3 = Color3.new(1, 1, 1)
+	label.TextSize = 14
+	label.ZIndex = 10
+	label.TextXAlignment = Enum.TextXAlignment.Left
+	table.insert(shade2,button)
+	table.insert(text1,label)
+	return button
+end
+
+ColorsButton = makeSettingsButton("Edit Theme","rbxassetid://4911962991")
+ColorsButton.Position = UDim2.new(0,5,0,55)
+ColorsButton.Size = UDim2.new(1,-10,0,25)
+ColorsButton.Name = "Colors"
+ColorsButton.Parent = SettingsHolder
+
+Keybinds = makeSettingsButton("Edit Keybinds","rbxassetid://129697930")
+Keybinds.Position = UDim2.new(0, 5, 0, 85)
+Keybinds.Size = UDim2.new(1, -10, 0, 25)
+Keybinds.Name = "Keybinds"
+Keybinds.Parent = SettingsHolder
+
+Aliases = makeSettingsButton("Edit Aliases","rbxassetid://5147488658")
+Aliases.Position = UDim2.new(0, 5, 0, 115)
+Aliases.Size = UDim2.new(1, -10, 0, 25)
+Aliases.Name = "Aliases"
+Aliases.Parent = SettingsHolder
+
+StayOpen.Name = "StayOpen"
+StayOpen.Parent = SettingsHolder
+StayOpen.BackgroundColor3 = Color3.fromRGB(46, 46, 47)
+StayOpen.BorderSizePixel = 0
+StayOpen.BackgroundTransparency = 1
+StayOpen.Position = UDim2.new(0, 5, 0, 30)
+StayOpen.Size = UDim2.new(1, -10, 0, 20)
+StayOpen.Font = Enum.Font.SourceSans
+StayOpen.TextSize = 14
+StayOpen.Text = "Keep Menu Open"
+StayOpen.TextColor3 = Color3.new(1, 1, 1)
+StayOpen.TextXAlignment = Enum.TextXAlignment.Left
+StayOpen.ZIndex = 10
+table.insert(shade2,StayOpen)
+table.insert(text1,StayOpen)
